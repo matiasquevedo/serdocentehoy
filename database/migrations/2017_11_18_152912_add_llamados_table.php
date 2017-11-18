0ba21617d6ebf343cc948af0d0b4b02f96bcb459
Matias <matiasquevedo.sabbath@gmail.com>
2017-11-18 13:06:26 -0300

    agrego migraciones

---
+++ database/migrations/2017_11_18_152912_add_llamados_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLlamadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('llamados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('institucion_id')->unsigned();//datos de la escuela
+            $table->integer('area_id')->unsigned();//matematica, fisica...
+            $table->string('titulo');
+            $table->string('fecha');
+            $table->string('hora');
+            $table->string('orden');//primer llamdo, segundo, etc.            
+            $table->string('horario_catedra');//horario en que se tiene que dar clases
+            $table->string('descripcion');//
+            $table->timestamps();
+
+            //llaves
+            $table->foreign('institucion_id')->references('id')->on('instituciones')->onDelete('cascade');
+            $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_11_18_154748_add_instituciones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddInstitucionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('instituciones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');->unique();
+            $table->string('numero');->unique();
+            $table->string('ubicacion');->unique();
+            $table->enum('nivel',['nivel inicial','primaria','secundaria','nivel terciario','universidad']);
+            $table->enum('tipo',['especial','albergue']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
